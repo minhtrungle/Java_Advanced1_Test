88e41291f205d795ff4a26b9273eed0bbd87f169
minhtrungle <minhtrungle21599@gmail.com>
2023-03-08 15:19:22 +0700

    Update Ex1

--- src/main/java/Exercise1/Circle.java
+++ src/main/java/Exercise1/Circle.java
@@ -7,6 +7,34 @@ public class Circle {
     }
 
     public Circle(double radius) {
+        if (radius <= 0) {
+            throw new RuntimeException("Error");
+        }
         this.radius = radius;
+    }
+
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius <= 0) {
+            throw new RuntimeException("Error");
+        }
+        this.radius = radius;
+    }
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
     }
 }
--- src/main/java/Exercise1/Main.java
+++ src/main/java/Exercise1/Main.java
@@ -1,5 +1,27 @@
 package Exercise1;
 
 public class Main {
+    //Test LMT
+    public static void main(String[] args) {
+        //Khởi tạo theo Circle()
+        Circle c1 = new Circle(6);
+        System.out.println(c1);
 
+        Circle c2 = new Circle();
+        System.out.println(c2);
+
+        c1.setRadius(5);
+        System.out.println(c1);
+        System.out.println("Bán kính là: " + c1.getRadius());
+
+        System.out.printf("Diện tích là: %.3f%n", c1.getArea());
+        System.out.printf("Chu vi là: %.3f%n", c1.getCircumference());
+        try{
+            Circle c3 = new Circle(-2);
+            c3.setRadius(-3);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+    }
 }
