954179a902403d82a05b180e635e13fe159207f4
minhtrungle <minhtrungle21599@gmail.com>
2023-03-08 15:03:27 +0700

    Update Ex1

---
+++ src/main/java/Exercise1/Circle.java
@@ -0,0 +1,12 @@
+package Exercise1;
+
+public class Circle {
+    private double radius = 1.0;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
