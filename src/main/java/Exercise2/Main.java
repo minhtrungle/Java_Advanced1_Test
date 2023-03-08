919c01b9a6e0cd9c1d49fcb4c6c0b5683da72e53
minhtrungle <minhtrungle21599@gmail.com>
2023-03-08 20:59:41 +0700

    Update Ex2, 3

--- src/main/java/Exercise2/Main.java
+++ src/main/java/Exercise2/Main.java
@@ -8,36 +8,43 @@ sẵn dữ liệu và 1 đối tượng nhập từ bàn phím, sau đó in ra t
 public class Main {
     public static void main(String[] args) {
         //Dữ liệu có sẵn
-        NhanVien nv1 = new KyThuat("20173422", "Le Minh Trung", "21/05/1999", "Thanh Hoa", "Programmer");
+        KyThuat k1 = new KyThuat("20173422", "Le Minh Trung", "21/05/1999", "Thanh Hoa", "Programmer");
 
         System.out.println("Thông tin nhân viên: ");
 
-        System.out.println(nv1);
-        String result1 = nv1.moTaCongViec();
+        System.out.println(k1);
+        String result1 = k1.moTaCongViec();
         System.out.println(result1);
 
         //Dữ liệu nhập từ bàn phím
+        KyThuat k2 = new KyThuat();
+
         Scanner in = new Scanner(System.in);
+        String s1, s2, s3, s4, s5;
 
         System.out.println("Nhập mã nhân viên: ");
-        String maNhanVien = in.nextLine();
+        s1 = in.nextLine();
+        k2.setMaNhanVien(s1);
 
         System.out.println("Nhập họ tên: ");
-        String hoTen = in.nextLine();
+        s2 = in.nextLine();
+        k2.setHoTen(s2);
 
         System.out.println("Nhập năm sinh: ");
-        String namSinh = in.nextLine();
+        s3 = in.nextLine();
+        k2.setNamSinh(s3);
 
         System.out.println("Nhập địa chỉ: ");
-        String diaChi = in.nextLine();
+        s4 = in.nextLine();
+        k2.setDiaChi(s4);
 
         System.out.println("Nhập chuyên ngành: ");
-        String chuyenNganh = in.nextLine();
+        s5 = in.nextLine();
+        k2.setChuyenNghanh(s5);
 
-        NhanVien nv2 = new KyThuat(maNhanVien, hoTen, namSinh, diaChi, chuyenNganh);
 
-        System.out.println(nv2);
-        String result2 = nv2.moTaCongViec();
+        System.out.println(k2);
+        String result2 = k2.moTaCongViec();
         System.out.println(result2);
     }
 }
--- src/main/java/Exercise3/Main.java
+++ src/main/java/Exercise3/Main.java
@@ -3,24 +3,29 @@ package Exercise3;
 public class Main {
     public static void main(String[] args) {
         VanBan v = new VanBan("le minh trung");
+
         // đếm số từ của văn bản
-        System.out.println("So tu la:");
-        System.out.println(v.demSoTuVanBan("le minh trung"));
+        int count = (int) v.demSoTuVanBan("le minh trung");
+        System.out.println("Co so tu la: " + count);
 
         // chuẩn hoá một xâu theo tiêu chuẩn
-        v.setKyTu(v.chuanHoaVanBanTieuChuan());
+        String s1 = v.chuanHoaVanBanTieuChuan();
+        v.setKyTu(s1);
         System.out.println(v);
 
         // chuyển đổi toàn bộ văn bản thành viết hoa.
-        v.setKyTu(v.toanBoVanBanVietHoa());
+        String s2 = v.toanBoVanBanVietHoa();
+        v.setKyTu(s2);
         System.out.println(v);
 
         // chuyển đổi toàn bộ văn bản thành viết thường.
-        v.setKyTu(v.toanBoVanBanVietThuong());
+        String s3 = v.toanBoVanBanVietThuong();
+        v.setKyTu(s3);
         System.out.println(v);
 
         // viết hoa chữ cái đầu tiên của mỗi từ trong văn bản.
-        v.setKyTu(v.vietHoaChuCaiDau("le minh trung"));
+        String s4 = v.vietHoaChuCaiDau("le minh trung");
+        v.setKyTu(s4);
         System.out.println(v);
     }
 }
